ff7908fef76ce8fe05437a363d0c03d0dabd81ac
Christopher Berner <christopherberner@gmail.com>
2019-08-25 17:09:33 -0700

    Add benchmarks

---
+++ benches/decode_benchmark.rs
@@ -0,0 +1,60 @@
+use rand::Rng;
+use std::time::Instant;
+use erasure_coding::{Encoder, Decoder};
+
+const TARGET_TOTAL_BYTES: usize = 128 * 1024 * 1024;
+const DATA_SHARD_COUNTS: [usize; 8] = [3, 7, 10, 30, 70, 100, 150, 200];
+const REPAIR_SHARD_COUNTS: [usize; 8] = [2, 2, 3, 5, 10, 10, 15, 20];
+
+fn black_box(value: u64) {
+    if value == rand::thread_rng().gen() {
+        println!("{}", value);
+    }
+}
+
+fn benchmark(shard_size: u16) -> u64 {
+    let mut black_box_value = 0;
+    for (data_shards, repair_shards) in DATA_SHARD_COUNTS.iter().zip(REPAIR_SHARD_COUNTS.iter()) {
+        let elements = data_shards * shard_size as usize;
+        let mut data: Vec<u8> = vec![0; elements];
+        for i in 0..elements {
+            data[i] = rand::thread_rng().gen();
+        }
+
+        let iterations = TARGET_TOTAL_BYTES / elements;
+        let encoder = Encoder::new(*data_shards as u8, *repair_shards as u8);
+        let (_, repair) = encoder.encode(&data);
+        let mut erased_datas = vec![];
+        for _ in 0..iterations {
+            let mut erased: Vec<Option<u8>> = data.iter().map(|x| Some(*x)).collect();
+            for _ in 0..*repair_shards {
+                let i = rand::thread_rng().gen_range(0, elements);
+                erased[i] = None;
+            }
+            erased_datas.push(erased);
+        }
+        let now = Instant::now();
+        for i in 0..iterations {
+            let decoder = Decoder::new(*data_shards as u8, *repair_shards as u8);
+            let result = decoder.decode(&erased_datas[i], &repair);
+            black_box_value += result[0] as u64;
+        }
+        let elapsed = now.elapsed();
+        let elapsed = elapsed.as_secs() as f64 + elapsed.subsec_millis() as f64 * 0.001;
+        let throughput = (elements * iterations * 8) as f64 / 1024.0 / 1024.0 / elapsed;
+        println!("data shards = {}, repair shards = {}, decoded {} MB in {:.3}secs, throughput: {:.1}Mbit/s",
+                 data_shards,
+                 repair_shards,
+                 elements * iterations / 1024 / 1024,
+                 elapsed,
+                 throughput);
+    }
+
+    return black_box_value;
+}
+
+fn main() {
+    let symbol_size = 1;
+    println!("Shard size: {} bytes", symbol_size);
+    black_box(benchmark(symbol_size));
+}
---
+++ benches/encode_benchmark.rs
@@ -0,0 +1,44 @@
+use rand::Rng;
+use std::time::Instant;
+use erasure_coding::Encoder;
+
+const TARGET_TOTAL_BYTES: usize = 128 * 1024 * 1024;
+const DATA_SHARD_COUNTS: [usize; 8] = [3, 7, 10, 30, 70, 100, 150, 200];
+const REPAIR_SHARD_COUNTS: [usize; 8] = [2, 2, 3, 5, 10, 10, 15, 20];
+
+fn black_box(value: u64) {
+    if value == rand::thread_rng().gen() {
+        println!("{}", value);
+    }
+}
+
+fn main() {
+    let mut black_box_value = 0;
+    let shard_size = 1;
+    println!("Symbol size: {} bytes", shard_size);
+    for (data_shards, repair_shards) in DATA_SHARD_COUNTS.iter().zip(REPAIR_SHARD_COUNTS.iter()) {
+        let elements = data_shards * shard_size as usize;
+        let mut data: Vec<u8> = vec![0; elements];
+        for i in 0..elements {
+            data[i] = rand::thread_rng().gen();
+        }
+
+        let now = Instant::now();
+        let iterations = TARGET_TOTAL_BYTES / elements;
+        for _ in 0..iterations {
+            let encoder = Encoder::new(*data_shards as u8, *repair_shards as u8);
+            let (_, repair) = encoder.encode(&data);
+            black_box_value += repair[0] as u64;
+        }
+        let elapsed = now.elapsed();
+        let elapsed = elapsed.as_secs() as f64 + elapsed.subsec_millis() as f64 * 0.001;
+        let throughput = (elements * iterations * 8) as f64 / 1024.0 / 1024.0 / elapsed;
+        println!("data shards = {}, repair shards = {}, encoded {} MB in {:.3}secs, throughput: {:.1}Mbit/s",
+                 data_shards,
+                 repair_shards,
+                 elements * iterations / 1024 / 1024,
+                 elapsed,
+                 throughput);
+    }
+    black_box(black_box_value);
+}
